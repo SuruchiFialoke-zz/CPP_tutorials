d89e1854fd58e491c8a06009817719563dd2c49e
Suruchi_Upenn <ssuruchi@seas.upenn.edu>
2017-03-06 00:51:20 -0500

    Added a new Xcode project on Functions

---
+++ Test4/Test4/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Test4
+//
+//  Created by Suruchi on 3/6/17.
+//  Copyright © 2017 Suruchi. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+
+using namespace std ;
+
+int main() {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
