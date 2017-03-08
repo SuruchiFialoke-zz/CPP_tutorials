82d973ea60e71c71c33f6f287a4a01790c91534a
Suruchi_Upenn <ssuruchi@seas.upenn.edu>
2017-03-07 23:47:37 -0500

    modified Test3

--- Test3/Test3/main.cpp
+++ Test3/Test3/main.cpp
@@ -1,6 +1,7 @@
 //
 //  main.cpp
-//  Trying  Switch, global constants and precision 
+//  Trying  Switch
+// global constants and precision
 //
 //  Created by Suruchi on 3/6/17.
 //  Copyright © 2017 Suruchi. All rights reserved.
@@ -8,11 +9,17 @@
 
 #include <iostream>
 #include <iomanip>
+#include <ctime>
 
+//ctime allows to use random seeds
 using namespace std ;
 
 //Constants
 const double TAX_FEDERAL = 0.1511111 ;
+
+//Static variables
+static int x = 10;
+void modifyNum() ;
 
 int main() {
     double yourIncome ;
@@ -27,8 +34,8 @@ int main() {
     cout.precision(2) ;
     
     //can also do it with iomanip
-    cout << "you willl make (2 decimal): "<<yourIncome<<endl;
-    
+    cout << "you willl make (rounded to 2 decimal places): "<<yourIncome<<'\n';
+    cout<<"\n";
     
     //Trying switch statements
     int gpa_round = 3;
@@ -48,5 +55,27 @@ int main() {
             cout<<"You fail"<<endl ;
     }
     
+    cout<<"\n";
+    cout<<"\n";
+
+    
+    //Trying ctime
+    
+    srand(time(0)) ;
+
+    cout<<"A random number: "<<rand()<<"\n" ;
+    cout<<"A random number form 1 to x: "<<rand() % (x+1)<<"\n" ;
+
+    cout<<"round 1; \n" ;
+    
+    modifyNum() ;
+    cout<<"round 2; \n" ;
+    modifyNum() ;
+
     return 0;
+}
+
+void modifyNum() {
+
+    cout<< (x += 5)<<"\n" ;
 }
