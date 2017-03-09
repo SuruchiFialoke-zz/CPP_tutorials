f740bc3aeb2f65ddd6ce73b7a2a9e175a8072a45
Suruchi_Upenn <ssuruchi@seas.upenn.edu>
2017-03-08 22:31:50 -0500

    Added Test11 on Recursive Functions and Filestream

--- Test11/Test11/main.cpp
+++ Test11/Test11/main.cpp
@@ -1,15 +1,73 @@
 //
 //  main.cpp
 //  Test11
-//
+//  Contains simple recursive Functions
 //  Created by Suruchi on 3/8/17.
 //  Copyright © 2017 Suruchi. All rights reserved.
 //
 
 #include <iostream>
+using namespace std ;
 
-int main(int argc, const char * argv[]) {
+void counter(int) ;
+// counter - a recursive function that counts up /down
+// @param int - a number to increment or decrement
+// basically does what a for loop does
+
+int factorial(int) ;
+// factorial - a recursive function that returns factorial of a number
+
+int fibonacci(int) ;
+//fibonacci - prints a fibonacci series upto an integer
+
+int main() {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int num ;
+    cout<<"ENter a number: \n" ;
+    cin>>num ;
+    
+    counter(num) ;
+    
+    cout<<"\n" ;
+    cout<<"Factorial: "<<factorial(num)<<"\n" ;
+    
+    cout<<"Fibonacci "<<fibonacci(num)<<"\n" ;
     return 0;
 }
+
+//Counter
+void counter (int count) {
+    // always needs a base case to stop: using a for loop
+    // the subsequent values should lead to base case
+    if(count >= 0) {
+        counter (count -1) ;
+        cout<<" \n Number: "<<count<<" " ;
+        //flip the order of the above statments to change the order
+    }
+    //cout<<"Done with your number!!\n" ;
+}
+
+//Factorial
+
+int factorial (int num) {
+    if(num<=1) return 1 ;
+    else return num*factorial(num-1) ;
+
+}
+
+
+//Fibonacci
+
+int fibonacci (int num) {
+    //int result ;
+    if(num<2) {
+          return num ;
+    }
+    else {
+        return ( fibonacci(num-1) + fibonacci(num-2) ) ;
+    }
+    
+}
+
+
+
